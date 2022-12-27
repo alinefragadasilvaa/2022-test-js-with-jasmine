d5487feac193ceb1580bd51c01577997affcf994
NevesRS <88750273+NevesRS@users.noreply.github.com>
2022-12-27 16:41:02 -0300

    Update CategoriaComunidadeSpec.js

--- jasmine-standalone-4.5.0/spec/CategoriaComunidadeSpec.js
+++ jasmine-standalone-4.5.0/spec/CategoriaComunidadeSpec.js
@@ -41,4 +41,60 @@ describe("Criar uma categoria de comunidade", function(){
              expect(resultado.criar()).toEqual("Categoria criada com sucesso!")
         })
     })
+    describe("Verificar nome inválido", function(){
+        it("deve avisar que o nome é inválido", function(){
+            let resultado = new CategoriaComunidade ("P", "", "brunoneves");
+            expect(resultado.criar()).toEqual("Nome inválido, , ")
+        })
+        it("deve avisar que o nome é inválido", function(){
+            let resultado = new CategoriaComunidade ("Oi", "", "brunoneves");
+            expect(resultado.criar()).toEqual("Nome inválido, , ")
+        })
+        it("deve avisar que o nome é inválido", function(){
+            let resultado = new CategoriaComunidade ("Tri", "", "brunoneves");
+            expect(resultado.criar()).toEqual("Nome inválido, , ")
+        })
+        it("deve avisar que o nome é inválido", function(){
+            let resultado = new CategoriaComunidade ("Aprenda a abrir uma empresa barata e lucrativa em 6 meses", "", "brunoneves");
+            expect(resultado.criar()).toEqual("Nome inválido, , ")
+        })
+        it("deve avisar que o nome é inválido", function(){
+            let resultado = new CategoriaComunidade ("Marketing 1", "", "brunoneves");
+            expect(resultado.criar()).toEqual("Nome inválido, , ")
+        })
+        it("deve avisar que o nome é inválido", function(){
+            let resultado = new CategoriaComunidade ("12", "", "brunoneves");
+            expect(resultado.criar()).toEqual("Nome inválido, , ")
+        })
+        it("deve avisar que o nome é inválido", function(){
+            let resultado = new CategoriaComunidade ("@", "", "brunoneves");
+            expect(resultado.criar()).toEqual("Nome inválido, , ")
+        })
+    });
+    describe("Verificar descrição inválida", function(){
+        it("deve avisar que a descrição é inválida", function(){
+            let resultado = new CategoriaComunidade ("Papo", "p", "brunoneves");
+            expect(resultado.criar()).toEqual(", Descrição inválida, ")
+        })
+        it("deve avisar que a descrição é inválida", function(){
+            let resultado = new CategoriaComunidade ("Papo", "Gestão dos negócios", "brunoneves");
+            expect(resultado.criar()).toEqual(", Descrição inválida, ")
+        })
+        it("deve avisar que a descrição é inválida", function(){
+            let resultado = new CategoriaComunidade ("Papo", "Aprenda a controlar as finanças da sua empresa e reservar fundos para investimentos, assim como eu fiz, e veja sua vida melhorar de forma significativa! Aqui você encontra relatos, dicas, tutoriais, que irão mudar o dia a dia da sua empresa. Aumente sua produtividade!", "brunoneves");
+            expect(resultado.criar()).toEqual(", Descrição inválida, ")
+        })
+        it("deve avisar que a descrição é inválida", function(){
+            let resultado = new CategoriaComunidade ("Papo", "12", "brunoneves");
+            expect(resultado.criar()).toEqual(", Descrição inválida, ")
+        })
+        it("deve avisar que a descrição é inválida", function(){
+            let resultado = new CategoriaComunidade ("Papo", "@", "brunoneves");
+            expect(resultado.criar()).toEqual(", Descrição inválida, ")
+        })
+        it("deve avisar que a descrição é inválida", function(){
+            let resultado = new CategoriaComunidade ("Papo", "1@", "brunoneves");
+            expect(resultado.criar()).toEqual(", Descrição inválida, ")
+        })
+    })
 });
