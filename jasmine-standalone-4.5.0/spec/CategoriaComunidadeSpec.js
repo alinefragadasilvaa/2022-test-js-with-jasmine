6cbd51ea6d9d62ba4718cf32ca27e80439fe3762
Aline Fraga da Silva <109742288+alinefragadasilvaa@users.noreply.github.com>
2022-12-27 16:36:07 -0300

    Update CategoriaComunidadeSpec.js

--- jasmine-standalone-4.5.0/spec/CategoriaComunidadeSpec.js
+++ jasmine-standalone-4.5.0/spec/CategoriaComunidadeSpec.js
@@ -1,5 +1,5 @@
 describe("Criar uma categoria de comunidade", function(){
-    describe("Verificar campo nome em branco", function(){
+    describe("Verificar campos em branco", function(){
         it("deve avisar que há campo em branco", function(){
             let resultado = new CategoriaComunidade ("", "", "");
              expect(resultado.criar()).toEqual("Campos obrigatórios em branco!")
@@ -13,5 +13,32 @@ describe("Criar uma categoria de comunidade", function(){
              expect(resultado.criar()).toEqual("Campos obrigatórios em branco!")
         })
     })
-    
+    describe("Verificar apelido", function(){
+        it("deve avisar que o apelido é inválido", function(){
+            let resultado = new CategoriaComunidade ("Papo", "Faça o seu marketing", "mariazinha");
+             expect(resultado.criar()).toEqual(", , Apelido inválido")
+        })
+    })
+    describe("Criar categoria", function(){
+        it("deve avisar que a categoria foi criada", function(){
+            let resultado = new CategoriaComunidade ("Papo", "Aprenda a controlar as finanças da sua empresa e reservar fundos para investimentos", "brunoneves");
+             expect(resultado.criar()).toEqual("Categoria criada com sucesso!")
+        })
+        it("deve avisar que a categoria foi criada", function(){
+            let resultado = new CategoriaComunidade ("Papo", "Faça o seu marketing", "brunoneves");
+             expect(resultado.criar()).toEqual("Categoria criada com sucesso!")
+        })
+        it("deve avisar que a categoria foi criada", function(){
+            let resultado = new CategoriaComunidade ("Alug@", "", "brunoneves");
+             expect(resultado.criar()).toEqual("Categoria criada com sucesso!")
+        })
+        it("deve avisar que a categoria foi criada", function(){
+            let resultado = new CategoriaComunidade ("Controle Financeiro e Investimentos", "", "brunoneves");
+             expect(resultado.criar()).toEqual("Categoria criada com sucesso!")
+        })
+        it("deve avisar que a categoria foi criada", function(){
+            let resultado = new CategoriaComunidade ("Papo", "", "brunoneves");
+             expect(resultado.criar()).toEqual("Categoria criada com sucesso!")
+        })
+    })
 });
